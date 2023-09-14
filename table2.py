2f122cb44cea95cdaa849288fc268be6cf0baf5c
ytejeswa <tejeswariyegireddi22@gmail.com>
2023-09-14 14:36:40 +0530

    We have made some changes

--- table2.py
+++ table2.py
@@ -46,14 +46,7 @@ def main():
          .option("password", properties["password"]) \
          .option("driver", properties["driver"]) \
          .load()
-    #pandas_df = table_df.toPandas()
 
-    # Convert pandas DataFrame to a dictionary
-    #result_dict= pandas_df.to_dict(orient="records")
-    #values_list = list(result_dict.values())
-    #table_names = table_df.select("table_name").rdd.flatMap(lambda x: x).collect()
-
-    #table_list = list(table_names)
     values_list = [row[0] for row in table_df.select(col("table_name")).collect()]
 
     for i in values_list:
